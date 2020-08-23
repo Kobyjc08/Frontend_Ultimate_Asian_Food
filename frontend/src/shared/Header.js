028ba805e97e6e0f2c9aaf48e7d0d3258d65167e
Esteban <emedina@creatonico.com>
2020-08-23 10:33:15 +0200

    Added an alt tag to logo

--- frontend/src/shared/Header.js
+++ frontend/src/shared/Header.js
@@ -3,7 +3,7 @@ import logo from "../images/logo_190x104.jpg";
 export const Header = () => {
   return (
     <header>
-      <img src={logo} />
+      <img src={logo} alt="The Ultimate AsianFood Logo" />
     </header>
   );
 };
