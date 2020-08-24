34f44e0afdb545d6857a682d4ebd6b03a3fd73ad
Jorge Cobo <kobyjc@hotmail.com>
2020-08-24 21:33:03 +0200

    Create a carousel component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Carrousel from './components/Carrousel';
 import logo from "./logo.svg";
 import "./App.css";
 import { Layout } from "./layout/Layout";
 
 function App() {
-  return <Layout>Content</Layout>;
+  return <Layout>
+    <Carrousel />
+
+  </Layout>;
 }
 
 export default App;
