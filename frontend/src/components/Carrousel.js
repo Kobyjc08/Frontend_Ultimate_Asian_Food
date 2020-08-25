ff61b7c8566d5e9a38fe9b175202c822bc2b724c
Jorge Cobo <kobyjc@hotmail.com>
2020-08-25 13:27:45 +0200

    new images

--- frontend/src/components/Carrousel.js
+++ frontend/src/components/Carrousel.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Carousel } from 'react-bootstrap';
 import Firstcarrousel from "../images/Firstcarrousel.jpeg";
+import Secondcarousel from "../images/Secondcarousel.jpeg";
+import thirdcarousel from "../images/thirdcarousel.jpeg";
 
 const CarouselComponent = () => (
-
+<div>
 <Carousel>
   <Carousel.Item>
     <img
@@ -15,28 +17,24 @@ const CarouselComponent = () => (
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
-      alt="Third slide"
+      src={Secondcarousel}
+      alt="Second slide"
     />
-
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
+      src={thirdcarousel}
       alt="Third slide"
     />
 
     <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <h3></h3>
+      <p></p>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
+</div>
 )
 
 export default CarouselComponent;
