a39a65a3a19a97d35f7bfb49b70218245002e7b8
Jorge Cobo <kobyjc@hotmail.com>
2020-08-25 13:23:10 +0200

    new carousel images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Carrousel from './components/Carrousel';
+import {Container} from 'react-bootstrap'
 import logo from "./logo.svg";
 import "./App.css";
 import { Layout } from "./layout/Layout";
 
 function App() {
   return <Layout>
+    <Container fluid className="carrousel_wrap">
     <Carrousel />
+    </Container>
+   
 
   </Layout>;
 }
