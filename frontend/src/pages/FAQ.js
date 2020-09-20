5b1f3aaf9ffa14afaacc901fc2f2613de814c487
Esteban <emedina@creatonico.com>
2020-09-20 22:23:05 +0200

    added questions and answers to FAQ page

--- frontend/src/pages/FAQ.js
+++ frontend/src/pages/FAQ.js
@@ -93,7 +93,7 @@ const FAQ = () => {
                 as={Button}
                 style={{ color: "#faf28e" }}
                 variant="link"
-                eventKey="1"
+                eventKey="2"
               >
                 What payment methods can I use?
               </Accordion.Toggle>
@@ -101,6 +101,118 @@ const FAQ = () => {
             <Accordion.Collapse eventKey="2">
               <Card.Body>
                 You can use all the major credit cards and Pay Pal.
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card
+            style={{
+              backgroundColor: "#8d1a20",
+              color: "white",
+              marginTop: "10px",
+            }}
+          >
+            <Card.Header>
+              <Accordion.Toggle
+                as={Button}
+                style={{ color: "#faf28e" }}
+                variant="link"
+                eventKey="3"
+              >
+                What are the delivery charges?
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="3">
+              <Card.Body>
+                Delivery charges are dependent on the shipment requirements. If
+                the products on your order are due to special requirements (for
+                example specialty foods) extra fee will be added to the shipment
+                charges. You can see the shipping fees on the checkout process
+                before the payment is made.
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card
+            style={{
+              backgroundColor: "#8d1a20",
+              color: "white",
+              marginTop: "10px",
+            }}
+          >
+            <Card.Header>
+              <Accordion.Toggle
+                as={Button}
+                style={{ color: "#faf28e" }}
+                variant="link"
+                eventKey="4"
+              >
+                How can I get offer of bulk amounts?
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="4">
+              <Card.Body>
+                When you are logged in and you add products to shopping cart,
+                you have the ability to send offer request to us using the
+                shopping cart. You just need to add the products and quantities
+                you are interested in and click “offer request”. We will send
+                you an offer. Please note that this feature should only be used
+                for bulk quantity price requests.
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card
+            style={{
+              backgroundColor: "#8d1a20",
+              color: "white",
+              marginTop: "10px",
+            }}
+          >
+            <Card.Header>
+              <Accordion.Toggle
+                as={Button}
+                style={{ color: "#faf28e" }}
+                variant="link"
+                eventKey="5"
+              >
+                Do I have to order online?
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="5">
+              <Card.Body>
+                You can also send your order to order@ultimateasianfood.es. You
+                can also order using by calling (+34 607 596 485). Online
+                ordering is preferred in most cases because by ordering online,
+                you will save time, you will have easier payment process and all
+                the information about the order will be accessible for you
+                anytime. Also if you want to make same order later, you can use
+                “reorder” feature.
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card
+            style={{
+              backgroundColor: "#8d1a20",
+              color: "white",
+              marginTop: "10px",
+            }}
+          >
+            <Card.Header>
+              <Accordion.Toggle
+                as={Button}
+                style={{ color: "#faf28e" }}
+                variant="link"
+                eventKey="6"
+              >
+                How to contact customer service?
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="6">
+              <Card.Body>
+                If you have question regarding our online store (ordering,
+                account questions, technical questions), please contact
+                help@ultimateasianfood.es.
+                <br />
+                On pricing and shipping related issues you can contact customer
+                support at support@ultimateasianfood.es or call +34 607 596 485.
               </Card.Body>
             </Accordion.Collapse>
           </Card>
