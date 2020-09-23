20c25291ef50b32f9b19bd52d5f4a17a56206ff6
ehsanwaheedraja <ihsan_raja@live.com>
2020-09-23 15:53:18 +0200

    dashboard changes

--- frontend/server/routes/dashboard.js
+++ frontend/server/routes/dashboard.js
@@ -5,18 +5,12 @@ const pool = require("../db");
 router.post("/", authorize, async (req, res) => {
   try {
     const user = await pool.query(
-      "SELECT firstname FROM users WHERE user_id = $1",
+      "SELECT firstname, lastname,user_email,address,dni,orders.id,order_items.quantity,products.product_name,order_date      FROM users join orders on users.user_id = orders.customer_id join order_items on orders.id = order_items.order_id join products on order_items.product_id = products.id WHERE user_id = $1",
       [req.user.id]
     );
 
-    //if would be req.user if you change your payload to this:
-
-    //   function jwtGenerator(user_id) {
-    //   const payload = {
-    //     user: user_id
-    //   };
-
-    res.json(user.rows[0]);
+    console.log(user.rows[0]);
+    res.json(user.rows);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Card } from "react-bootstrap";
 import { Breadcrumb } from "react-bootstrap";
 import { Layout } from "../layout/Layout";
+import faqimg from "../images/faq_icon.png";
+// import EditTodo from "../components/EditTodo";
 // import { toast } from "react-toastify";
 
 const Dashboard = ({ setAuth }) => {
   const [name, setName] = useState("");
+  const [name2, setName2] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Address, setAddress] = useState("");
+  const [DNI, setDNI] = useState("");
+  const [orders, setorders] = useState([]);
 
   const getProfile = async () => {
     try {
@@ -15,7 +22,12 @@ const Dashboard = ({ setAuth }) => {
       });
 
       const parseData = await res.json();
-      setName(parseData.firstname);
+      setName(parseData[0].firstname);
+      setName2(parseData[0].lastname);
+      setEmail(parseData[0].user_email);
+      setAddress(parseData[0].address);
+      setDNI(parseData[0].dni);
+      setorders(parseData);
     } catch (err) {
       console.error(err.message);
     }
@@ -43,100 +55,56 @@ const Dashboard = ({ setAuth }) => {
           <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
 
           <Breadcrumb.Item id="breadcrumb_links_vertical" active>
-            DashBoard
+            Account
           </Breadcrumb.Item>
         </Breadcrumb>
-        <div>
-          <h1 className="mt-5">Dashboard</h1>
-          <h2>Welcome {name}</h2>
-          <h3>Soon your orders will appear here!!!</h3>
-          <button onClick={(e) => logout(e)} className="btn btn-primary">
-            Logout
-          </button>
+        <div className="img-product">
+          <img src={faqimg} alt="FAQ Icon" />
         </div>
+        <h2 className="red-letters">Account</h2>
+        <h2 className="mt-0 mb-1 new-titles">Order History</h2>
+        <div className="dashboard-class ">
+          <table>
+            <thead className="thead-light">
+              <tr>
+                <th className="mt-0 mb-1 new-titles">Order ID </th>
+                <th className="mt-0 mb-1 new-titles">Quantity </th>
+                <th className="mt-0 mb-1 new-titles">Product Name </th>
+                <th className="mt-0 mb-1 new-titles">Order Date </th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => (
+                <tr key={order.id}>
+                  <td>{order.id}</td>
+                  <td>{order.quantity}</td>
+                  <td>{order.product_name}</td>
+                  <td>{order.order_date}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Card style={{ width: "18rem" }} className="customCard">
+            <Card.Body>
+              <Card.Title>Account</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Your account details
+              </Card.Subtitle>
+              <Card.Text>
+                Full Name: {name} {name2}
+              </Card.Text>
+              <Card.Text>Email Address: {Email}</Card.Text>
+              <Card.Text>Address: {Address}</Card.Text>
+              <Card.Text>DNI: {DNI}</Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <button onClick={(e) => logout(e)} className="btn btn-primary">
+          Logout
+        </button>
       </Container>
     </Layout>
   );
 };
 
 export default Dashboard;
-
-//not working version to render customer order details
-
-// import React, { useEffect, useState } from "react";
-// import { Container } from "react-bootstrap";
-// import { Breadcrumb } from "react-bootstrap";
-// import { Layout } from "../layout/Layout";
-// import axios from "axios";
-// // import { toast } from "react-toastify";
-
-// const Dashboard = ({ setAuth }) => {
-//   const [name, setName] = useState("");
-//   const [customerID, setcustomerID] = useState("");
-
-//   const getProfile = async () => {
-//     try {
-//       const res = await fetch("http://localhost:5000/dashboard/", {
-//         method: "POST",
-//         headers: { jwt_token: localStorage.token },
-//       });
-
-//       const parseData = await res.json();
-//       setName(parseData.firstname);
-//       setcustomerID(parseData.user_id)
-//     } catch (err) {
-//       console.error(err.message);
-//     }
-//   };
-
-//   const logout = async (e) => {
-//     e.preventDefault();
-//     try {
-//       localStorage.removeItem("token");
-//       setAuth(false);
-//       //   toast.success("Logout successfully");
-//     } catch (err) {
-//       console.error(err.message);
-//     }
-//   };
-// Dashboard2();
-//   useEffect(() => {
-//     getProfile();
-//   }, []);
-
-//   const Dashboard2 = () => {
-//   const [getCustomerOrders, setgetCustomerOrders] = useState([]);
-
-//   useEffect(() => {
-//     axios
-//       .get("http://localhost:5000/orderItems/:"+{customerID})+"/"
-//       .then(function (response) {
-//         setgetCustomerOrders(response.data);
-//         console.group(response.data);
-//       });
-//   }, []);
-
-//   return (
-//     <Layout>
-//       <Container>
-//         <Breadcrumb>
-//           <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-
-//           <Breadcrumb.Item id="breadcrumb_links_vertical" active>
-//             DashBoard
-//           </Breadcrumb.Item>
-//         </Breadcrumb>
-//         <div>
-//           <h1 className="mt-5">Dashboard</h1>
-//           <h2>Welcome {name}</h2>
-//           <h3>Soon your orders will appear here!!!</h3>
-//           <button onClick={(e) => logout(e)} className="btn btn-primary">
-//             Logout
-//           </button>
-//         </div>
-//       </Container>
-//     </Layout>
-//   );
-// };
-
-// export default Dashboard;
