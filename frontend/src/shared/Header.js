507a09ff6c7194a2a4b19727a4cfa5cdf972735a
Esteban <emedina@creatonico.com>
2020-09-24 15:20:03 +0200

    modify responsive menu

--- frontend/src/shared/Header.js
+++ frontend/src/shared/Header.js
@@ -1,138 +1,64 @@
 import React from "react";
-import { Dropdown, DropdownButton } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, DropdownButton } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logoLg from "../images/logo_190x104.jpg";
 import logoSm from "../images/logo_120x67.jpg";
 export const Header = () => {
   return (
-    <header className="header mb-5">
-      <nav className="navbar navbar-expand-lg">
-        <div className="container">
-          <Link to="/" className="navbar-brand home">
-            <img
-              src={logoLg}
-              alt="The Ultimate Asian Food Eshop Logo"
-              className="d-none d-md-inline-block"
-            />
-            <img
-              src={logoSm}
-              alt="The Ultimate Asian Food Eshop Logo"
-              className="d-inline-block d-md-none"
-            />
-            <span className="sr-only">The Ultimate Asian Food Eshop</span>
+    <Navbar collapseOnSelect expand="lg" className="navbar2">
+      <Navbar.Brand href="#home">
+        <img
+          src={logoLg}
+          alt="The Ultimate Asian Food Eshop Logo"
+          className="d-none d-md-inline-block"
+        />
+        <img
+          src={logoSm}
+          alt="The Ultimate Asian Food Eshop Logo"
+          className="d-inline-block d-md-none"
+        />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Link to="/" className="nav-link2 active">
+            Home
           </Link>
-          <div className="navbar-buttons">
-            <button
-              type="button"
-              data-toggle="collapse"
-              data-target="#navigation"
-              className="btn btn-outline-secondary navbar-toggler"
-            >
-              <span className="sr-only">Toggle navigation</span>
-              <i className="fa fa-align-justify"></i>
-            </button>
-            <a
+          <Link to="/Login" className="nav-link2">
+            Login
+          </Link>
+          <NavDropdown title="Categories" id="collasible-nav-dropdown">
+            <NavDropdown.Item>
+              <Link to="/categories/Desserts">Desserts</Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <Link to="/categories/Traditional-Food">
+                Asian Traditional foods
+              </Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <Link to="/categories/Spices">Spices</Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <Link to="/categories/Lentils-Rice-Flour">
+                Lentils/Rice/Flour
+              </Link>
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <div className="navbar-buttons d-flex justify-content-end">
+            <Link
+              to="/ShoppingCart"
               href="basket.html"
-              className="btn btn-outline-secondary navbar-toggler"
+              className="btn btn-primary navbar-btn"
             >
               <i className="fa fa-shopping-cart"></i>
-            </a>
+              <span>0 items in cart</span>
+            </Link>
           </div>
-          <div id="navigation" className="collapse navbar-collapse">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to="/" className="nav-link active">
-                  Home
-                </Link>
-              </li>
-              {/* <li className="nav-item">
-                <Link to="#" className="nav-link">
-                  Search
-                </Link>
-              </li> */}
-              <li className="nav-item">
-                <Link to="/Login" className="nav-link">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/Register" className="nav-link">
-                  Register
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/About" className="nav-link">
-                  About
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to="/FAQ" className="nav-link">
-                  FAQ
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/Contact" className="nav-link">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-            {/* <div className="topnav">
-              <input
-                className="nav-item"
-                type="text"
-                placeholder="Search.."
-              ></input>
-            </div> */}
-            <Dropdown>
-              <DropdownButton className="nav-link" title="Categories">
-                <Dropdown.ItemText>Items by Categories</Dropdown.ItemText>
-                <Dropdown.Item as="button">
-                  <Link to="/categories/Desserts">Desserts</Link>
-                </Dropdown.Item>
-                <Dropdown.Item as="button">
-                  <Link to="/categories/Traditional-Food">
-                    Asian Traditional foods
-                  </Link>
-                </Dropdown.Item>
-                <Dropdown.Item as="button">
-                  <Link to="/categories/Spices">Spices</Link>
-                </Dropdown.Item>
-                <Dropdown.Item as="button">
-                  <Link to="/categories/Lentils-Rice-Flour">
-                    Lentils/Rice/Flour
-                  </Link>
-                </Dropdown.Item>
-              </DropdownButton>
-            </Dropdown>
-
-            <div className="navbar-buttons d-flex justify-content-end">
-              <Link to="/ShoppingCart" href="basket.html" className="btn btn-primary navbar-btn">
-                <i className="fa fa-shopping-cart"></i>
-                <span>0 items in cart</span>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </nav>
-      <div id="search" className="collapse">
-        <div className="container">
-          <form role="search" className="ml-auto">
-            <div className="input-group">
-              <input
-                type="text"
-                placeholder="Search"
-                className="form-control"
-              />
-              <div className="input-group-append">
-                <button type="button" className="btn btn-primary">
-                  <i className="fa fa-search"></i>
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </header>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
